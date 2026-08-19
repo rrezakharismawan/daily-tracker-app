e1ed15d8ae8b8e7ec839f58b797fd5f4e71d3326
rrezakharismawan <rrezakharismawan@gmail.com>
2026-08-19 13:43:42 +0700

    update list

--- config.js
+++ config.js
@@ -18,26 +18,31 @@ const CONFIG = {
   // 'murni'    = Warna Bar Putih (QC, CSQ)
   // 'sekunder' = Warna Bar Kuning (Sisa Tugas Lainnya)
   DROPDOWN_B: {
-    "---Select Task QC---":                     { divider: 0, type: "separator" },
-    "OCALC Update KMZ/DWG":                     { divider: 15, type: "sekunder" },
-    "OCALC Revision":                           { divider: 15, type: "sekunder" },
-    "Email Request Update":                     { divider: 15, type: "sekunder" },
-    "Email Request Revision":                   { divider: 15, type: "sekunder" },
-    "Overcapacity":                             { divider: 15, type: "sekunder" },
-    "Overcapacity - Create RCMD":     { divider: 15, type: "sekunder" },
+      "---Select Task Analyze---":      { divider: 0, type: "separator" },
+    "New Job Analysis Process":           { divider: 15, type: "sekunder" },  
+    "New Job Analysis Consistency":       { divider: 15, type: "sekunder" },
 
-        "---Select Task Other---":              { divider: 0, type: "separator" },
+      "---Select Task Overcapacity---": { divider: 0, type: "separator" },
+    "Overcapacity":                       { divider: 15, type: "sekunder" },
+    "Overcapacity - Create RCMD":         { divider: 15, type: "sekunder" },
+    "Extra Difficulty Jobs":              { divider: 15, type: "sekunder" },
+
+      "---Select Task Update---":           { divider: 0, type: "separator" },
+    "OCALC Update KMZ/DWG":           { divider: 15, type: "sekunder" },
+    "OCALC Revision":                 { divider: 15, type: "sekunder" },
+    "Email Request Update":           { divider: 15, type: "sekunder" },
+    "Email Request Revision":         { divider: 15, type: "sekunder" },
+
+      "---Select Task Other---":        { divider: 0, type: "separator" },
     "Administration dan Finalize":    { divider: 15, type: "sekunder" },
     "Meeting dan Info rules baru":    { divider: 15, type: "sekunder" },
     "Create Attachment for Email":    { divider: 15, type: "sekunder" },
     "Custom Task":                    { divider: 15, type: "sekunder" },
 
-        "---Select Task Analyze---":      { divider: 0, type: "separator" },
-    "New Job Analysis Process":           { divider: 15, type: "sekunder" },  
-    "Extra Difficulty (A2U/RELOC/BAU)":   { divider: 15, type: "sekunder" },
-    "Doublecheck and track request":      { divider: 15, type: "sekunder" },
+      "---Select Task Remove ---":     { divider: 0, type: "separator" },
+    "Training":                       { divider: 15, type: "sekunder" },
 
-    "---Select Task Pure Poles---":   { divider: 0, type: "separator" },
+      "---Select Task Pure Poles---":   { divider: 0, type: "separator" },
     "QC":                             { divider: 15, type: "murni" },
     "CSQ":                            { divider: 30, type: "murni" }
   }
